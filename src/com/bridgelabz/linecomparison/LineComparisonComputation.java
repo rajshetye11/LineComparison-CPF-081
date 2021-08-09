2c190bae36780450745d1f068b34c732d50e9f7f
rajshetye11 <rajshetye11@gmail.com>
2021-08-09 15:03:41 +0530

    [raj] : added Equality of Lines

--- src/com/bridgelabz/linecomparison/LineComparisonComputation.java
+++ src/com/bridgelabz/linecomparison/LineComparisonComputation.java
@@ -16,6 +16,16 @@ public class LineComparisonComputation {
 		Double Length2 = Math.sqrt((x4-x3)^2 +(y4-y3)^2);
 		System.out.println("Length 2 = " +Length2);  //UC1
 		
+		System.out.println();
+		
+		if (Length1.equals(Length2))
+		{
+			System.out.println("Length of both Lines are Equal");  //UC2
+		}
+		else
+		{
+			System.out.println("Length of both Lines are Not Equal");  //UC2
+		}
 		
 	}
 
